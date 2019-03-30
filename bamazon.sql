71c2d107c6de8e5644691f53c0062a39b1060511
mahtoutsaid <mahtoutsaid0913@gmail.com>
2019-03-30 00:03:22 -0700

    bamazon table created

---
+++ bamazon.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+      	id INT NOT NULL AUTO_INCREMENT,
+		product_name VARCHAR(255) NOT NULL, 
+		department_name VARCHAR(255) NOT NULL,
+		price INT NOT NULL, 
+		stock_quantity INT NOT NULL,
+		PRIMARY KEY(id)
+);
